98438b69e09289103bb241f4c4c31f0338a563da
Nitin Kumar <61423609+GeneralNitin@users.noreply.github.com>
2021-10-25 19:45:03 +0530

    Feat: Test cases and UI-Service (#13)
    * feat: add test-coverage report
    
    * refactor: sonarcloud config
    
    * refactor: sonarcloud properties
    
    * Feat: Added Product Service (#6)
    
    * feat: product service implementation
    
    * feat: added a common module for reused files (#7)
    
    * feat: added a common module for reused files
    
    * refactor: cleaned up all modules
    
    * feat: added test cases for OrderController (#11)
    
    * feat: base ui-service setup (#12)
    
    Co-authored-by: I514807 <nitin.kumar15@sap.com>
    Co-authored-by: Nitin Kumar <92848821+kumarniti4@users.noreply.github.com>

--- order-service/src/test/java/com/devops/orderservice/controller/OrderControllerTest.java
+++ order-service/src/test/java/com/devops/orderservice/controller/OrderControllerTest.java
@@ -1,14 +1,22 @@
 package com.devops.orderservice.controller;
 
+import com.devops.orderservice.dtos.request.OrderRequest;
+import com.devops.orderservice.dtos.response.OrderResponse;
+import com.devops.orderservice.dtos.response.ProductResponse;
 import com.devops.orderservice.service.OrderService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+import java.util.UUID;
 
 @WebMvcTest(controllers = OrderController.class)
 class OrderControllerTest {
@@ -22,19 +30,46 @@ class OrderControllerTest {
     @Test
     @DisplayName("Test Create Order Endpoint")
     void testCreateOrder() throws Exception {
-        int actual = 0;
-        assertEquals(0, actual);
-//        Orders order = new Orders("1", "test order", 1, 1);
-//        Mockito.when(service.createOrder(Mockito.any(Orders.class))).thenReturn(order);
-//
-//        mockMvc.perform(MockMvcRequestBuilders.post("/orders")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("{\n" +
-//                                "    \"name\":\"Mobile\",\n" +
-//                                "    \"qty\":\"2\",\n" +
-//                                "    \"price\":1000\n" +
-//                                "}"))
-//                .andExpect(MockMvcResultMatchers.status().is(201))
-//                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+        ProductResponse product1 = new ProductResponse("55a46d25-82d1-4c3e-a559-c7564fa59e32", 1, 990);
+        ProductResponse product2 = new ProductResponse("e35c05c3-5107-4f80-a8a7-35b34e37554b", 3, 499);
+        OrderResponse response = new OrderResponse(UUID.randomUUID().toString(), 4, 1489, Arrays.asList(product1, product2));
+
+        Mockito.when(service.createOrder(Mockito.any(OrderRequest.class))).thenReturn(response);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/orders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "    \"totalQty\":4,\n" +
+                                "    \"totalPrice\":1489,\n" +
+                                "    \"products\": [\n" +
+                                "        {\n" +
+                                "            \"id\": \"55a46d25-82d1-4c3e-a559-c7564fa59e32\",\n" +
+                                "            \"qty\": 1,\n" +
+                                "            \"price\": 990\n" +
+                                "        },\n" +
+                                "        {\n" +
+                                "            \"id\": \"e35c05c3-5107-4f80-a8a7-35b34e37554b\",\n" +
+                                "            \"qty\": 3,\n" +
+                                "            \"price\": 499\n" +
+                                "        }\n" +
+                                "    ]\n" +
+                                "}"))
+                .andExpect(MockMvcResultMatchers.status().is(201))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @DisplayName("Get Order by Id")
+    void testGetAOrder() throws Exception {
+
+        ProductResponse product1 = new ProductResponse("55a46d25-82d1-4c3e-a559-c7564fa59e32", 1, 990);
+        ProductResponse product2 = new ProductResponse("e35c05c3-5107-4f80-a8a7-35b34e37554b", 3, 499);
+        OrderResponse response = new OrderResponse(UUID.randomUUID().toString(), 4, 1489, Arrays.asList(product1, product2));
+
+        Mockito.when(service.findOrderById(Mockito.any(String.class))).thenReturn(response);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/orders/" + UUID.randomUUID()))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
     }
 }
