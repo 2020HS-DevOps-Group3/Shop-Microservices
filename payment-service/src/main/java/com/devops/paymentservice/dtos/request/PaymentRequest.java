f9498f21c76e6d35720bb7bb2df4faebe5d67e05
Nitin Kumar <61423609+GeneralNitin@users.noreply.github.com>
2021-10-26 01:06:25 +0530

    feat: implement payment service (#19)
    * Feature: product service and common module (#8)
    
    * feat: add test-coverage report
    
    * refactor: sonarcloud config
    
    * refactor: sonarcloud properties
    
    * Feat: Added Product Service (#6)
    
    * feat: product service implementation
    
    * feat: added a common module for reused files (#7)
    
    * feat: added a common module for reused files
    
    * refactor: cleaned up all modules
    
    * Feat: Test cases and UI-Service (#13)
    
    * feat: add test-coverage report
    
    * refactor: sonarcloud config
    
    * refactor: sonarcloud properties
    
    * Feat: Added Product Service (#6)
    
    * feat: product service implementation
    
    * feat: added a common module for reused files (#7)
    
    * feat: added a common module for reused files
    
    * refactor: cleaned up all modules
    
    * feat: added test cases for OrderController (#11)
    
    * feat: base ui-service setup (#12)
    
    Co-authored-by: I514807 <nitin.kumar15@sap.com>
    Co-authored-by: Nitin Kumar <92848821+kumarniti4@users.noreply.github.com>
    
    * feat: implement payment service (#17)
    
    * feat: add test-coverage report
    
    * refactor: sonarcloud config
    
    * refactor: sonarcloud properties
    
    * Feat: Added Product Service (#6)
    
    * feat: product service implementation
    
    * feat: added a common module for reused files (#7)
    
    * feat: added a common module for reused files
    
    * refactor: cleaned up all modules
    
    * feat: added test cases for OrderController (#11)
    
    * feat: base ui-service setup (#12)
    
    * feat: implement payment service (#16)
    
    Co-authored-by: I514807 <nitin.kumar15@sap.com>

--- payment-service/src/main/java/com/devops/paymentservice/dtos/request/PaymentRequest.java
+++ payment-service/src/main/java/com/devops/paymentservice/dtos/request/PaymentRequest.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentRequest {
 
+    @NotBlank
     private String orderId;
+
+    @NotBlank
+    private double totalPrice;
 }
--- payment-service/src/main/java/com/devops/paymentservice/service/PaymentService.java
+++ payment-service/src/main/java/com/devops/paymentservice/service/PaymentService.java
@@ -1,11 +1,18 @@
 package com.devops.paymentservice.service;
 
+import com.devops.common.enums.PaymentStatus;
 import com.devops.common.exceptions.PaymentAlreadyMadeException;
+import com.devops.common.exceptions.PaymentNotFoundException;
 import com.devops.common.utils.Constants;
+import com.devops.common.utils.ServiceUtils;
+import com.devops.paymentservice.dtos.request.PaymentRequest;
+import com.devops.paymentservice.dtos.response.PaymentResponse;
 import com.devops.paymentservice.entity.Payments;
 import com.devops.paymentservice.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -13,14 +20,28 @@ public class PaymentService {
 
     private final PaymentRepository repository;
 
-    public Payments makePayment(Payments payment) throws PaymentAlreadyMadeException {
-        if (this.paymentAlreadyMade(payment.getOrderId()))
+    public PaymentResponse makePayment(PaymentRequest request) throws PaymentAlreadyMadeException {
+        if (this.paymentAlreadyMade(request.getOrderId()))
             throw new PaymentAlreadyMadeException(Constants.ErrorMessages.PAYMENT_ALREADY_MADE);
-        else
-            return repository.save(payment);
+        else {
+            Payments payment = ServiceUtils.standardModelMapper(request, Payments.class);
+            payment.setStatus(this.getRandomStatus());
+            Payments savedPayment = repository.save(payment);
+            return ServiceUtils.standardModelMapper(savedPayment, PaymentResponse.class);
+        }
     }
 
     public boolean paymentAlreadyMade(String orderId) {
         return repository.existsByOrderId(orderId);
+    }
+
+    private PaymentStatus getRandomStatus() {
+        return new Random().nextBoolean() ? PaymentStatus.SUCCESS : PaymentStatus.FAILED;
+    }
+
+    public PaymentResponse getPaymentsByOrderId(String orderId) throws PaymentNotFoundException {
+        Payments payment = repository.findByOrderId(orderId)
+                .orElseThrow(() -> new PaymentNotFoundException(Constants.ErrorMessages.PAYMENT_NOT_FOUND));
+        return ServiceUtils.standardModelMapper(payment, PaymentResponse.class);
     }
 }
