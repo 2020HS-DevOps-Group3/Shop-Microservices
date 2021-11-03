d11b8eb72d4d70c8679f0f98d7b886273ed0f6e0
I514807 <nitin.kumar15@sap.com>
2021-11-03 08:48:28 +0530

    feat: order-service unit tests

--- order-service/src/test/java/com/devops/orderservice/repository/OrdersRepositoryTest.java
+++ order-service/src/test/java/com/devops/orderservice/repository/OrdersRepositoryTest.java
@@ -1,10 +1,11 @@
 package com.devops.orderservice.repository;
 
+import com.devops.orderservice.entity.Orders;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 //@ActiveProfiles("test")
@@ -15,10 +16,8 @@ class OrdersRepositoryTest {
 
     @Test
     void saveOrder() {
-        int actual = 0;
-        assertEquals(0, actual);
-//        Orders order = new Orders(null, "test order", 1, 100);
-//        Orders savedOrder = repository.save(order);
-//        assertThat(savedOrder).usingRecursiveComparison().ignoringFields("id").isEqualTo(order);
+        Orders order = new Orders(null, 1, 100);
+        Orders savedOrder = repository.save(order);
+        assertThat(savedOrder).usingRecursiveComparison().ignoringFields("id").isEqualTo(order);
     }
 }
