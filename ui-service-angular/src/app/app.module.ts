04c3e71872659a08845c57cfb43059c6d225bc0a
I514807 <nitin.kumar15@sap.com>
2021-11-07 19:43:06 +0530

    feat:angular: removed mat-ui, added bootstrap, backend integration pending

--- ui-service-angular/src/app/app.module.ts
+++ ui-service-angular/src/app/app.module.ts
@@ -11,10 +11,28 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ShoppingCartComponent } from './components/shoping-cart/shopping-cart.component';
+import { FiltersComponent } from './components/shoping-cart/filters/filters.component';
+import { ProductListComponent } from './components/shoping-cart/product-list/product-list.component';
+import { CartComponent } from './components/shoping-cart/cart/cart.component';
+import { CartItemComponent } from './components/shoping-cart/cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './components/shoping-cart/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ShoppingCartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartComponent,
+    CartItemComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
