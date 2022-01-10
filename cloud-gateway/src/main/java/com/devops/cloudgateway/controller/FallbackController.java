4cc9efe51e2aa5e5e7dc49860fbbec9ead812c37
Nitin Kumar <nitin.kumar15@sap.com>
2022-01-10 23:10:13 +0530

    fix: sonar security bugs

--- cloud-gateway/src/main/java/com/devops/cloudgateway/controller/FallbackController.java
+++ cloud-gateway/src/main/java/com/devops/cloudgateway/controller/FallbackController.java
@@ -1,23 +1,23 @@
 package com.devops.cloudgateway.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class FallbackController {
 
-    @RequestMapping("/order-fallback")
+    @GetMapping("/order-fallback")
     public Mono<String> orderServiceFallback() {
         return Mono.just("Order Service taking too long to respond. Please try again later");
     }
 
-    @RequestMapping("/product-fallback")
+    @GetMapping("/product-fallback")
     public Mono<String> productServiceFallback() {
         return Mono.just("Product Service taking too long to respond. Please try again later");
     }
 
-    @RequestMapping("/payment-fallback")
+    @GetMapping("/payment-fallback")
     public Mono<String> paymentServiceFallback() {
         return Mono.just("Payment Service taking too long to respond. Please try again later");
     }
